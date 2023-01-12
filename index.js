f2c6486d8c29ae042fcf479550dd291906ef2f86
idev009 <idev0091@gmail.com>
2023-01-12 20:59:22 +0530

    put,delete,post route handler added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const genres = [
   { id: 1, name: "Action" },
   { id: 2, name: "Horror" },
@@ -8,7 +10,7 @@ const genres = [
 ];
 
 // Routes
-
+// GET
 app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
@@ -25,6 +27,52 @@ app.get("/api/genres/:id", (req, res) => {
   }
 });
 
+//POST
+
+app.post("/api/genres", (req, res) => {
+  const genre = {
+    id: genres.length + 1,
+    name: req.body.name,
+  };
+
+  genres.push(genre);
+  res.send(genre);
+});
+
+app.put("/api/genres/:id", (req, res) => {
+  const genre = genres.find(function (c) {
+    return c.id === parseInt(req.params.id);
+  });
+  console.log(genre);
+  if (!genre) return res.status(404).send("Genre with given id not found");
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find(function (c) {
+    return c.id === parseInt(req.params.id);
+  });
+  if (!genre) return res.status(404).send("Genre with given id not found");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+app.delete("/api/genres/:id", (req, res) => {
+  const genre = genres.find((c) => c.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
 const port = process.env.Port || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+console.log(genres);
