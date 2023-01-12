c70eaba18b634130abd401439ea7ea93ca36d3e1
idev009 <idev0091@gmail.com>
2023-01-12 20:04:15 +0530

    get routes added

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
 
+app.get("/api/genres/:id", (req, res) => {
+  const genre = genres.find(function (c) {
+    return c.id === parseInt(req.params.id);
+  });
+
+  if (!genre) {
+    res.status(404).send("Genre with given id is not found");
+  } else {
+    res.send(genre);
+  }
+});
+
 const port = process.env.Port || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
