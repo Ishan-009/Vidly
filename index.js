27e52b90c3495bcf5fe5e6a7d09abd7e1385f0b3
idev009 <idev0091@gmail.com>
2023-01-12 18:49:33 +0530

    Starting Building Routes, /api/genres created

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const genres = [
   { id: 2, name: "Horror" },
   { id: 3, name: "Romance" },
 ];
+
+// Routes
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+const port = process.env.Port || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
